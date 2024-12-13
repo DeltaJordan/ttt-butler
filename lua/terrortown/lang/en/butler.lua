cebbef1e15fbfd9e5f9133d718737efc6c4f58ca
Jordan Marine <marine.jordan64@proton.me>
2024-12-13 16:29:06 -0600

    Added missing translations for Retired.

--- lua/terrortown/lang/en/butler.lua
+++ lua/terrortown/lang/en/butler.lua
@@ -1,12 +1,18 @@
 local L = LANG.GetLanguageTableReference("en")
 
 L[BUTLER.name] = "Butler"
+L["target_" .. BUTLER.name] = "Butler"
+L["ttt2_desc_" .. BUTLER.name] = [[The Butler needs to win with their Players team.]]
 L["info_popup_" .. BUTLER.name] = [[You are a Butler!
 Try to protect your Player..]]
 L["body_found_" .. BUTLER.abbr] = "They were Butler."
 L["search_role_" .. BUTLER.abbr] = "This person was a Butler!"
-L["target_" .. BUTLER.name] = "Butler"
-L["ttt2_desc_" .. BUTLER.name] = [[The Butler needs to win with their Players team]]
+
+L[RETIRED.name] = "Retired"
+L["target_" .. RETIRED.name] = "Retired"
+L["ttt2_desc_" .. RETIRED.name] = "[[You have retired, and can no longer affect the game.]]"
+L["body_found_" .. RETIRED.abbr] = "They were Retired."
+L["search_role_" .. RETIRED.abbr] = "This person was Retired!"
 
 L["tooltip_butler_fail_score"] = "Butler Failed: {score}"
 L["butler_fail_score"] = "Butler Failed:"
